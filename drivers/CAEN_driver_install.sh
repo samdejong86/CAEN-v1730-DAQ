cbe9e50d52ca8383e2aee35a54f4f052f7aa8d3f
Sam de Jong <samdejong86@gmail.com>
2019-01-17 15:59:22 -0800

    Fixes to CAEN_driver_install.sh

--- drivers/CAEN_driver_install.sh
+++ drivers/CAEN_driver_install.sh
@@ -10,44 +10,57 @@ errorVME="$(ls CAENVME* 2>&1)"
 if [[ $errorVME = *"No such file"* ]]; then
     echo "CAEN VME library not found!"
 else
-   cd CAENVME*/lib
-   echo "installing CAEN VME library"
-   sh install_x64
-   cd $OLDPWD
+    a=$(find ./ -type d -name "CAENVME*")
+    cd $a/lib
+    echo
+    echo "installing CAEN VME library"
+    pwd
+    sudo sh install_x64
+    cd $OLDPWD
 
-   errorUSB="$(ls CAENUSB* 2>&1)"
-   if [[ $errorUSB = *"No such file"* ]]; then
-       echo "CAEN USB library not found!"
-   else
-       cd CAENUSB*
+    errorUSB="$(ls CAENUSB* 2>&1)"
+    if [[ $errorUSB = *"No such file"* ]]; then
+	echo "CAEN USB library not found!"
+    else
+       a=$(find ./ -type d -name "CAENUSB*")
+       cd $a
+       echo
        echo "installing CAEN USB library"
+       pwd
        make
-       make install
+       sudo make install
        cd $OLDPWD
 
        errorComm="$(ls CAENComm* 2>&1)"
        if [[ $errorComm = *"No such file"* ]]; then
 	   echo "CAEN Comm library not found!"
        else
-	   cd CAENComm*/lib
+	   a=$(find ./ -type d -name "CAENComm*")
+	   cd $a/lib
+	   echo
 	   echo "installing CAEN Comm library"
-	   sh install_x64
+	   pwd
+	   sudo sh install_x64
 	   cd $OLDPWD
 	   
 	   errorDigi="$(ls CAENDigitizer* 2>&1)"
 	   if [[ $errorComm = *"No such file"* ]]; then
 	       echo "CAEN Digitizer library not found!"
 	   else
-	       cd CAENDigitizer*
+	       a=$(find ./ -type d -name "CAENDigitizer*")
+	       cd $a
+	       pwd
+	       echo
 	       echo "installing CAEN Digitizer library"
-	       sh install_64
+	       sudo sh install_64
 	       cd $OLDPWD
 	   fi
        fi	   
    fi
 fi
 
-
+#remove temporary directories
+rm -r ./CAEN*/
 
 
 
